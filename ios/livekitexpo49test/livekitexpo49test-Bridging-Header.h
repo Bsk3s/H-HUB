1f6288e8deef6be3183c227bf02705a3a346ced3
Bsk3s <benralph02@gmail.com>
2025-08-02 09:47:26 +0100

    Initial commit - comprehensive iOS build fixes

--- ios/livekitexpo49test/livekitexpo49test-Bridging-Header.h
+++ ios/livekitexpo49test/livekitexpo49test-Bridging-Header.h
@@ -3,3 +3,13 @@
 //
 
 #import <TargetConditionals.h>
+#import <Foundation/Foundation.h>
+
+// Ensure TARGET_OS_SIMULATOR is properly defined for all builds
+#ifndef TARGET_OS_SIMULATOR
+  #if TARGET_IPHONE_SIMULATOR
+    #define TARGET_OS_SIMULATOR 1
+  #else
+    #define TARGET_OS_SIMULATOR 0
+  #endif
+#endif
